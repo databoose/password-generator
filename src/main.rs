e0325fccf859a128a3f774aac6a3ef35def2cd3a
databoose <data@jp.com>
2022-05-05 04:33:03 -0700

    original template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+fn trim_newline(s: &mut String) {
+    while s.ends_with('\n') || s.ends_with('\r') {
+        s.pop();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin,stdout,Write};
+    
+    print!("\x1B[2J\x1B[1;1H"); // clear screen	
+    println!("----------------------------");
+    println!("---- Password Generator ----");
+    println!("----------------------------");
+    println!("\n");
+    
+    let mut input = String::new();
+    print!("Enter string length : ");
+    stdout().flush(); // print!() by itself doesnt flush stdout for some reason
+    stdin().read_line(&mut input)
+    	.ok()
+        .expect("Failed to read line");
+    trim_newline(&mut input); // for mutable variables we also pass &mut <variable_here>
+    let length;
+    match input.parse::<i32>() {
+	  Ok(n) => {
+	     length = n;
+	     //println!("{}", length);
+	  },
+	  Err(e) => println!("Error parsing, {}", e)
+    }
 }
