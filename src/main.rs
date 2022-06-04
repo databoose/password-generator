e1c8da88d13e25b0dd2ebc9c3e453c7316fbe9b6
databoose <data@jp.com>
2022-06-03 22:09:38 -0700

    replaced random library to allow for proper random uniform distribution, brought back length input

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
 
-fn get_random_buf() -> Result<[u8; 50], getrandom::Error> { // gives an array of ints, first parameter is type, second is amount of ints
-    let mut buf = [0u8; 50];
-    getrandom::getrandom(&mut buf)?;
-    Ok(buf)
+use rand::Rng;
+use std::io::{stdout,stdin,Write};
+
+fn trim_newline(s: &mut String) {
+    while s.ends_with('\n') || s.ends_with('\r') {
+        s.pop();
+    }
 }
 
 fn main() {  
@@ -15,23 +18,24 @@ fn main() {
     println!("----------------------------");
     println!("\n");
 
+    let mut input = String::new();
+    print!("Enter string length : ");
+    stdout().flush(); // print!() by itself doesnt flush stdout for some reason
+    stdin().read_line(&mut input)
+    	.ok()
+        .expect("Failed to read line");
+    trim_newline(&mut input); // for mutable variables we also pass &mut <variable_here>
+    let length = input.parse::<u16>().unwrap();
+
     let dict = vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 
                      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','O','R','S','T','U','V','W','X','Y','Z'
                      ,'!','@','#','$','%','^','&','*','(',')','-','_','+','=',
                      '1','2','3','4','5','6','7','8','9']; //75 chars
-    let randarr = match get_random_buf() {
-        Ok(arr) =>  arr, // we want each element value from the array to represent a char
-        Err(_) => panic!("error getting random bytes"),
-     };
-
     let mut random_passsword = String::new();
-    for i in 0..randarr.len() {
-        match randarr[i] {
-            0..=74 => {
-                random_passsword.push(dict[randarr[i] as usize]);
-            },
-            75..=255 => {},
-        }
+    let mut rng = rand::thread_rng();
+
+    for x in 0..length {
+        random_passsword.push(dict[rng.gen_range(0..75) as usize]);
     }
     println!("{}", random_passsword);
 }
